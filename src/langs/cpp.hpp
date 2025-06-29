a5d69580d788bf0dcb5b73b0a96f86963b1c3beb
zirosi <199428698+zirosi@users.noreply.github.com>
2025-06-28 17:29:36 -0700

    added support for --cmake

--- src/langs/cpp.hpp
+++ src/langs/cpp.hpp
@@ -11,8 +11,17 @@ class cpp {
 public:
   int makeCppProject(int argc, char** argv) {
     int returnValue = 0;
+    std::set<std::string> args;
 
-    returnValue = newSrcBuild(std::string(argv[2]));
+    for (int i = 3; i < argc; i++) {
+      args.insert(std::string(argv[i]));
+    }
+  
+    if (argc <= 3) {
+      returnValue = newSrcBuild(std::string(argv[2]));
+    } if (argc > 3) {
+      checkArgs(args, std::string(argv[2]));
+    }
 
     return returnValue;
   }
@@ -79,8 +88,8 @@ private:
     return returnValue;
   }
 
-  int addMainCpp(std::string srcPath) {
-    std::string mainCppPath = srcPath + "/main.cpp";
+  int addMainCpp(std::string mainCppPath) {
+    int returnValue = 0;
 
     std::ofstream mainCppFile(mainCppPath);
 
@@ -92,18 +101,57 @@ private:
 
     mainCppFile.close();
 
-    return 0;
+    if (checkExsist(mainCppPath) == 1) {
+      returnValue = 1;
+    }
+
+    return returnValue;
+  }
+
+  int addMakefile(std::string projectName, std::string mainCppPath, std::string buildPath) {
+    int returnValue = 0;
+
+    std::string makefilePath = projectName + "/makefile";
+
+    std::ofstream makefileFile(makefilePath);
+
+    makefileFile << "main:\n";
+    makefileFile << "  g++ " << mainCppPath << " -o " << buildPath;
+  
+    makefileFile.close();
+
+    returnValue = checkExsist(makefilePath);
+
+    return returnValue;
+  }
+
+  int addCmakelists(std::string projectName, std::string mainCppPath, std::string buildPath) {
+    int returnValue = 0;
+
+    std::string cmakelistsPath = projectName + "/CmakeLists.txt";
+
+    std::ofstream cmakelistsFile(cmakelistsPath);
+
+    cmakelistsFile << "cmake_minimum_required(VERSION 3.15)\n\n";
+    cmakelistsFile << "project( " << projectName << " )\n\n";
+    cmakelistsFile << "add_executable( " << projectName << " " << mainCppPath << " )";
+
+    cmakelistsFile.close();
+
+    returnValue = checkExsist(cmakelistsPath);
+
+    return returnValue;
   }
 
   int newSrcBuild(std::string projectName) {
     int returnValue = 0;
 
-    std::string srcPath = projectName + "/src";
+    std::string mainCppPath = projectName + "/src/main.cpp"; 
 
     int projectMade = makeProjectFolder(projectName);
     int srcMade = makeSrcFolder(projectName);
     int buildMade = makeBuildFolder(projectName);
-    int mainMade = addMainCpp(srcPath);
+    int mainMade = addMainCpp(mainCppPath);
 
     if (projectMade == 1) {
       returnValue = 1;
@@ -117,7 +165,26 @@ private:
 
     return returnValue;
   }
+  
+  int checkArgs(std::set<std::string> args, std::string projectName) {
+    int returnValue = 0;
 
+    std::set<std::string> SrcBuildMakeArgs = {"--make"};
+    std::set<std::string> SrcBuildCmakeArgs = {"--cmake"};
+
+    if (args == SrcBuildMakeArgs) {
+      if (newSrcBuild(projectName) == addMakefile(projectName, "src/main.cpp", "build/main.out") && newSrcBuild(projectName) == 0) {
+        returnValue = 0;
+      }
+    }
+    if (args == SrcBuildCmakeArgs) {
+      if (newSrcBuild(projectName) == addCmakelists(projectName, "src/main.cpp", "build/main.out") && newSrcBuild(projectName) == 0) {
+        returnValue = 0;
+      }
+    }
+
+    return returnValue;
+  }
 };
 
 #endif // !CPP
